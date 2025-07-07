ccad6d20532ccc83fa89e6ec263075f4af1c4a0b
georgeklaus <164487115+georgeklaus@users.noreply.github.com>
2025-07-07 13:08:32 +0300

    vercel configurations

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+sys.path.insert(0, '/var/task')  # Force Lambda to look here first
+print(sys.path)  # Check deployment logs for this output
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
